e310668b6174601268b943452e1ad0fbc6b0d010
Victoria Bruno <victoria.bruno14@gmail.com>
2023-12-05 11:33:18 +0100

    fix: :construction: Divide into components, import API data dynamically, convert temperature unit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css"
-import Weather from './components/Weather'
-import Search from "./components/Search";
+import Search from "./components/Search/Search";
+
 
 function App() {
-  return <div className="App">
-    <Search />
-    <Weather />
+
+return <div className="App">
+    <Search defaultCity="Bari"/>
     <br></br>
     <div className="open-source">Open source project available on <a href="https://github.com/Victoria-Bruno/my-react-weather-app.git">GitHub</a></div>
     </div>
